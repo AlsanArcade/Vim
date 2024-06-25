4a1e951a8212517c37f7b4b41f0741829bbe6d32
AlsanArcade <alsandritter@web.de>
2024-06-25 16:12:33 +0200

    deleted untraveled branches in mamba class

--- mamba-1p1p1/mamba_ssm/modules/mamba_simple.py
+++ mamba-1p1p1/mamba_ssm/modules/mamba_simple.py
@@ -127,42 +127,33 @@ class Mamba(nn.Module):
         self.D._no_weight_decay = True
 
         # bidirectional
-        if bimamba_type == "v1":
-            A_b = repeat(
-                torch.arange(1, self.d_state + 1, dtype=torch.float32, device=device),
-                "n -> d n",
-                d=self.d_inner,
-            ).contiguous()
-            A_b_log = torch.log(A_b)  # Keep A_b_log in fp32
-            self.A_b_log = nn.Parameter(A_b_log)
-            self.A_b_log._no_weight_decay = True
-        elif bimamba_type == "v2":
-            A_b = repeat(
-                torch.arange(1, self.d_state + 1, dtype=torch.float32, device=device),
-                "n -> d n",
-                d=self.d_inner,
-            ).contiguous()
-            A_b_log = torch.log(A_b)  # Keep A_b_log in fp32
-            self.A_b_log = nn.Parameter(A_b_log)
-            self.A_b_log._no_weight_decay = True 
+        
+        A_b = repeat(
+            torch.arange(1, self.d_state + 1, dtype=torch.float32, device=device),
+            "n -> d n",
+            d=self.d_inner,
+        ).contiguous()
+        A_b_log = torch.log(A_b)  # Keep A_b_log in fp32
+        self.A_b_log = nn.Parameter(A_b_log)
+        self.A_b_log._no_weight_decay = True 
 
-            self.conv1d_b = nn.Conv1d(
-                in_channels=self.d_inner,
-                out_channels=self.d_inner,
-                bias=conv_bias,
-                kernel_size=d_conv,
-                groups=self.d_inner,
-                padding=d_conv - 1,
-                **factory_kwargs,
-            )
+        self.conv1d_b = nn.Conv1d(
+            in_channels=self.d_inner,
+            out_channels=self.d_inner,
+            bias=conv_bias,
+            kernel_size=d_conv,
+            groups=self.d_inner,
+            padding=d_conv - 1,
+            **factory_kwargs,
+        )
 
-            self.x_proj_b = nn.Linear(
-                self.d_inner, self.dt_rank + self.d_state * 2, bias=False, **factory_kwargs
-            )
-            self.dt_proj_b = nn.Linear(self.dt_rank, self.d_inner, bias=True, **factory_kwargs)
+        self.x_proj_b = nn.Linear(
+            self.d_inner, self.dt_rank + self.d_state * 2, bias=False, **factory_kwargs
+        )
+        self.dt_proj_b = nn.Linear(self.dt_rank, self.d_inner, bias=True, **factory_kwargs)
 
-            self.D_b = nn.Parameter(torch.ones(self.d_inner, device=device))  # Keep in fp32
-            self.D_b._no_weight_decay = True
+        self.D_b = nn.Parameter(torch.ones(self.d_inner, device=device))  # Keep in fp32
+        self.D_b._no_weight_decay = True
 
         self.out_proj = nn.Linear(self.d_inner, self.d_model, bias=bias, **factory_kwargs)
 
@@ -193,119 +184,42 @@ class Mamba(nn.Module):
         A = -torch.exp(self.A_log.float())  # (d_inner, d_state)
         # In the backward pass we write dx and dz next to each other to avoid torch.cat
         if self.use_fast_path and inference_params is None:  # Doesn't support outputting the states
-            if self.bimamba_type == "v1":
-                A_b = -torch.exp(self.A_b_log.float())
-                out = bimamba_inner_fn(
-                    xz,
-                    self.conv1d.weight,
-                    self.conv1d.bias,
-                    self.x_proj.weight,
-                    self.dt_proj.weight,
-                    self.out_proj.weight,
-                    self.out_proj.bias,
-                    A,
-                    A_b,
-                    None,  # input-dependent B
-                    None,  # input-dependent C
-                    self.D.float(),
-                    delta_bias=self.dt_proj.bias.float(),
-                    delta_softplus=True,
-                )    
-            elif self.bimamba_type == "v2":
-                A_b = -torch.exp(self.A_b_log.float())
-                out = mamba_inner_fn_no_out_proj(
-                    xz,
-                    self.conv1d.weight,
-                    self.conv1d.bias,
-                    self.x_proj.weight,
-                    self.dt_proj.weight,
-                    A,
-                    None,  # input-dependent B
-                    None,  # input-dependent C
-                    self.D.float(),
-                    delta_bias=self.dt_proj.bias.float(),
-                    delta_softplus=True,
-                )
-                out_b = mamba_inner_fn_no_out_proj(
-                    xz.flip([-1]),
-                    self.conv1d_b.weight,
-                    self.conv1d_b.bias,
-                    self.x_proj_b.weight,
-                    self.dt_proj_b.weight,
-                    A_b,
-                    None,
-                    None,
-                    self.D_b.float(),
-                    delta_bias=self.dt_proj_b.bias.float(),
-                    delta_softplus=True,
-                )
-                # F.linear(rearrange(out_z, "b d l -> b l d"), out_proj_weight, out_proj_bias)
-                if not self.if_devide_out:
-                    out = F.linear(rearrange(out + out_b.flip([-1]), "b d l -> b l d"), self.out_proj.weight, self.out_proj.bias)
-                else:
-                    out = F.linear(rearrange(out + out_b.flip([-1]), "b d l -> b l d") / 2, self.out_proj.weight, self.out_proj.bias)
-
-            else:
-                out = mamba_inner_fn(
-                    xz,
-                    self.conv1d.weight,
-                    self.conv1d.bias,
-                    self.x_proj.weight,
-                    self.dt_proj.weight,
-                    self.out_proj.weight,
-                    self.out_proj.bias,
-                    A,
-                    None,  # input-dependent B
-                    None,  # input-dependent C
-                    self.D.float(),
-                    delta_bias=self.dt_proj.bias.float(),
-                    delta_softplus=True,
-                )
-        else:
-            x, z = xz.chunk(2, dim=1)
-            # Compute short convolution
-            if conv_state is not None:
-                # If we just take x[:, :, -self.d_conv :], it will error if seqlen < self.d_conv
-                # Instead F.pad will pad with zeros if seqlen < self.d_conv, and truncate otherwise.
-                conv_state.copy_(F.pad(x, (self.d_conv - x.shape[-1], 0)))  # Update state (B D W)
-            if causal_conv1d_fn is None:
-                x = self.act(self.conv1d(x)[..., :seqlen])
-            else:
-                assert self.activation in ["silu", "swish"]
-                x = causal_conv1d_fn(
-                    x=x,
-                    weight=rearrange(self.conv1d.weight, "d 1 w -> d w"),
-                    bias=self.conv1d.bias,
-                    activation=self.activation,
-                )
-
-            # We're careful here about the layout, to avoid extra transposes.
-            # We want dt to have d as the slowest moving dimension
-            # and L as the fastest moving dimension, since those are what the ssm_scan kernel expects.
-            x_dbl = self.x_proj(rearrange(x, "b d l -> (b l) d"))  # (bl d)
-            dt, B, C = torch.split(x_dbl, [self.dt_rank, self.d_state, self.d_state], dim=-1)
-            dt = self.dt_proj.weight @ dt.t()
-            dt = rearrange(dt, "d (b l) -> b d l", l=seqlen)
-            B = rearrange(B, "(b l) dstate -> b dstate l", l=seqlen).contiguous()
-            C = rearrange(C, "(b l) dstate -> b dstate l", l=seqlen).contiguous()
-            assert self.activation in ["silu", "swish"]
-            y = selective_scan_fn(
-                x,
-                dt,
+            A_b = -torch.exp(self.A_b_log.float())
+            out = mamba_inner_fn_no_out_proj(
+                xz,
+                self.conv1d.weight,
+                self.conv1d.bias,
+                self.x_proj.weight,
+                self.dt_proj.weight,
                 A,
-                B,
-                C,
+                None,  # input-dependent B
+                None,  # input-dependent C
                 self.D.float(),
-                z=z,
                 delta_bias=self.dt_proj.bias.float(),
                 delta_softplus=True,
-                return_last_state=ssm_state is not None,
             )
-            if ssm_state is not None:
-                y, last_state = y
-                ssm_state.copy_(last_state)
-            y = rearrange(y, "b d l -> b l d")
-            out = self.out_proj(y)
+            out_b = mamba_inner_fn_no_out_proj(
+                xz.flip([-1]),
+                self.conv1d_b.weight,
+                self.conv1d_b.bias,
+                self.x_proj_b.weight,
+                self.dt_proj_b.weight,
+                A_b,
+                None,
+                None,
+                self.D_b.float(),
+                delta_bias=self.dt_proj_b.bias.float(),
+                delta_softplus=True,
+            )
+            # F.linear(rearrange(out_z, "b d l -> b l d"), out_proj_weight, out_proj_bias)
+            if not self.if_devide_out:
+                out = F.linear(rearrange(out + out_b.flip([-1]), "b d l -> b l d"), self.out_proj.weight, self.out_proj.bias)
+            else:
+                out = F.linear(rearrange(out + out_b.flip([-1]), "b d l -> b l d") / 2, self.out_proj.weight, self.out_proj.bias)
+
+            
+        else:
+            assert False
         if self.init_layer_scale is not None:
                 out = out * self.gamma    
         return out
